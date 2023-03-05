052f39431a80fbb5893916a1bc5391e6ed8788ae
Giovanni Bedoya <giovedoya@gmail.com>
2023-03-05 13:05:42 +0100

    'Done'

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -7,15 +7,44 @@ export default function Counter() {
 
   // First iteration
 
+  const handleIncrease = () => {
+    setCount(prev => prev + 1)
+
+  }
+
+  const handleDecrease = () => {
+    setCount(prev => prev - 1)
+  }
+
   // Second and Fourth iteration
 
+
+
+  useEffect(() => {
+    setCount(Math.floor(Math.random() * 11))
+    return () => {
+      setCount(0)
+      setDynamicClass('text')
+    }
+    }, [])
+
   // Third iteration
+
+  useEffect(() => {
+    if (count <= 0) {
+      setDynamicClass('red')
+    } else if (count >0 && count < 5) {
+      setDynamicClass('orange')
+    } else if (count > 5) {
+      setDynamicClass('green')
+    }
+  }, [count])
 
   return (
     <div>
       <p>Counter: <span className={dynamicClass}>{count}</span></p>
-      <button>+ Increase</button>
-      <button>- Decrease</button>
+      <button onClick={handleIncrease}>+ Increase</button>
+      <button onClick={handleDecrease}>- Decrease</button>
     </div>
   )
 }
